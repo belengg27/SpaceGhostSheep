dd33670dbaf3de3b1da970a0d93d2f3c43ce63c5
Liototo <74362833+Liototo@users.noreply.github.com>
2022-04-08 14:47:37 +0200

    Added sound effects when winning/losing points

--- Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
+++ Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
@@ -42,11 +42,19 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
     public void addPoint()
     {
         points++;
+        playSound("winPoint");
     }
 
     public void removePoint()
     {
         points--;
+        playSound("LosePoint");
+    }
+
+    public void playSound(string sound)
+    {
+        AudioSource audio = gameObject.AddComponent<AudioSource>();
+        audio.PlayOneShot((AudioClip)Resources.Load(sound));
     }
 
 }
