79bd5724db7ac1f26ba54248c4484dac15c2cc6d
vfaltings <victor.faltings@epfl.ch>
2022-04-09 21:28:54 +0200

    implemented end game screen

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class GameManager : MonoBehaviour
 
     public void StartGame() {
         Time.timeScale = 1;
+    }
+
+    public void RestartGame() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("WelcomeScene");
     }
 
     // Update is called once per frame
