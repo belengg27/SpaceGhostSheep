6bfb4ab4e4eb9228c35cf1f84c83892b652fdad4
Liototo <74362833+Liototo@users.noreply.github.com>
2022-06-04 17:20:10 +0200

    Added random spawning of objects

--- Assets/Scripts/Maze Game/ItemBehavior.cs
+++ Assets/Scripts/Maze Game/ItemBehavior.cs
@@ -12,16 +12,29 @@ public class ItemBehavior : MonoBehaviour
 {
     public Item color;
 
+    private Vector3[] greenPos = { new Vector3(11f, -0.5f, -10.5f), 
+                                   new Vector3(17f, -0.5f, -14.5f), 
+                                   new Vector3(8f, -0.5f, -15f) };
+
+    private Vector3[] pinkPos = { new Vector3(20f, -0.5f, -11.5f),
+                                  new Vector3(5f, -0.5f, -17.5f),
+                                  new Vector3(17f, -0.5f, -5.5f) };
+
+    private Vector3[] goldPos = { new Vector3(2f, -0.5f, -5.5f),
+                                  new Vector3(26f, -0.5f, -2f),
+                                  new Vector3(8f, -0.5f, -2f), };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (color != Item.green) Deactivate();
+        else transform.position = greenPos[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -36,8 +49,39 @@ public class ItemBehavior : MonoBehaviour
         }
     }
 
+    void Activate()
+    {
+        gameObject.SetActive(true);
+        switch (color)
+        {
+            case Item.green:
+                transform.position = greenPos[Random.Range(0, greenPos.Length - 1)];
+                break;
+            case Item.pink:
+                transform.position = pinkPos[Random.Range(0, pinkPos.Length - 1)];
+                break;
+            case Item.gold:
+                transform.position = pinkPos[Random.Range(0, pinkPos.Length - 1)];
+                break;
+            default: break;
+        }
+    }
+
     void Deactivate()
     {
         gameObject.SetActive(false);
+        switch (color)
+        {
+            case Item.green:
+                Invoke("Activate", Random.Range(5f, 10f));
+                break;
+            case Item.pink:
+                Invoke("Activate", Random.Range(10f, 15f));
+                break;
+            case Item.gold:
+                Invoke("Activate", Random.Range(15f, 20f));
+                break;
+            default: break;
+        }
     }
 }
