fff5db4741a990dea065bc7a5d891e494e9258b4
vfaltings <victor.faltings@epfl.ch>
2022-06-11 14:33:29 +0200

    fixed pause bug

--- Assets/Scripts/Game/Pause.cs
+++ Assets/Scripts/Game/Pause.cs
@@ -6,17 +6,31 @@ public class Pause : MonoBehaviour
 {
     GameObject menu;
 
+    private bool hasStarted;
+
     // Start is called before the first frame update
     void Start()
     {
         menu = GameObject.Find("PauseMenu");
         menu.SetActive(false);
+        hasStarted = false;
+    }
+
+    public void startGame() 
+    {
+        hasStarted = true;
     }
 
     // Update is called once per frame
 
     void Update()
     {
+        // Don't do anything if the game hasn't started
+        if (!hasStarted) 
+        {
+            return;
+        }
+        
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             GameManager.PauseGame();
