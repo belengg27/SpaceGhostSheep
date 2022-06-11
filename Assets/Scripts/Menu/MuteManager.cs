93371ff5c485d38c76920a394ad5f669f78f5f85
belengg27 <belen01gomez@gmail.com>
2022-06-11 15:06:42 +0200

    checkmark changes when game is muted

--- Assets/Scripts/Menu/MuteManager.cs
+++ Assets/Scripts/Menu/MuteManager.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class MuteManager : MonoBehaviour
 {
     private bool isMuted;
+    public GameObject check; 
 
     void Start()
     {
         isMuted = PlayerPrefs.GetInt("MUTED") == 1;
         AudioListener.pause = isMuted;
+        UpdateCheck();
     }
 
     public void MutePressed()
@@ -15,6 +17,15 @@ public class MuteManager : MonoBehaviour
         isMuted = !isMuted;
         AudioListener.pause = isMuted;
         PlayerPrefs.SetInt("MUTED", isMuted ? 1 : 0);
+        UpdateCheck();
     }
 
+    public void UpdateCheck()
+    {
+        if (!isMuted) {
+            check.SetActive(true);
+        } else {
+            check.SetActive(false);
+        }
+    }
 }
