a5c6536ef73761db26705e25c036a8d08722238f
belengg27 <belen01gomez@gmail.com>
2022-06-11 14:55:42 +0200

    Mute manager in welcome scene

--- Assets/Scripts/Menu/MuteManager.cs
+++ Assets/Scripts/Menu/MuteManager.cs
@@ -6,13 +6,15 @@ public class MuteManager : MonoBehaviour
 
     void Start()
     {
-        isMuted = false;
+        isMuted = PlayerPrefs.GetInt("MUTED") == 1;
+        AudioListener.pause = isMuted;
     }
 
     public void MutePressed()
     {
         isMuted = !isMuted;
         AudioListener.pause = isMuted;
+        PlayerPrefs.SetInt("MUTED", isMuted ? 1 : 0);
     }
 
 }
